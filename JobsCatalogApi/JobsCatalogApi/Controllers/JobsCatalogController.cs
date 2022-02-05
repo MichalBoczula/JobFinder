5a5ebdeb991770c80b808077be3362b1d008a904
Michal Boczula <michal.boczula0101@gmail.com>
2022-02-05 15:28:07 +0100

    added list and details query

---
+++ JobsCatalogApi/JobsCatalogApi/Controllers/JobsCatalogController.cs
@@ -0,0 +1,30 @@
+﻿using JobsCatalog.Application.Features.Entities.JobDetails;
+using JobsCatalog.Application.Features.Entities.JobsList;
+using JobsCatalogApi.Common;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobsCatalogApi.Controllers
+{
+    [Route("api/jobs")]
+    [ApiController]
+    public class JobsCatalogController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetJobsList()
+        {
+            var vm = await Mediator.Send(new JobsListQuery());
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetJobDetails(int id)
+        {
+            var vm = await Mediator.Send(new JobDetailsQuery() { Id = id});
+            return Ok(vm);
+        }
+    }
+}
